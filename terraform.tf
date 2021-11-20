0140409b7d7561fae75f9211123f51e11412d139
ruslan3105 <93218266+ruslan3105@users.noreply.github.com>
2021-11-20 12:38:09 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,9 +3,48 @@ provider "aws" {
     secret_key = "0/Auh77myskPq/iLcK9zktcuJ67dXbG7H0I4OQKM"
     region = "eu-central-1"
 }
-
+variable "ingressrules"{
+    type = list(number)
+    default = [80,443]
+}
+variable "egressrules"{
+    type = list(number)
+    default = [80,443]
+}
 
 resource "aws_instance" "my_linux" {
     ami = "ami-030e490c34394591b"
     instance_type = "t2.micro"
+    security_groups = [aws_security_group.webtraffic.name]
+
+    tags = {
+        Name = "My trainning instance"
+    }
+}
+
+resource "aws_security_group" "webtraffic" {
+
+    name = "Allow traffic"
+
+    dynamic "ingress" {
+        iterator = port
+        for_each = var.ingressrules
+        content {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+        
+    }
+
+    dynamic "egress" {
+        iterator = port
+        for_each = var.egressrules
+        content {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
 }
