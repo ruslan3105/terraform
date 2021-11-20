7f8082dc136bfa14464513b644a833bccb0c35e4
ruslan3105 <93218266+ruslan3105@users.noreply.github.com>
2021-11-20 13:02:36 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -26,25 +26,17 @@ resource "aws_security_group" "webtraffic" {
 
     name = "Allow traffic"
 
-    dynamic "ingress" {
-        iterator = port
-        for_each = var.ingressrules
-        content {
-        from_port = port.value
-        to_port = port.value
+    ingress {
+        from_port = 443
+        to_port = 443
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
         }
         
-    }
-
-    dynamic "egress" {
-        iterator = port
-        for_each = var.egressrules
-        content {
-        from_port = port.value
-        to_port = port.value
+    egress {
+        from_port = 443
+        to_port = 443
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
         }
-}
+    }
